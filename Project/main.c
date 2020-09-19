82691e0d145524642d2ca20252af707703f47de8
99002588 <71487673+99002588@users.noreply.github.com>
2020-09-19 09:02:15 +0530

    Update main.c

--- Project/main.c
+++ Project/main.c
@@ -14,3 +14,16 @@ if((fp=fopen("employeeInfo.txt","rb+"))==NULL)
  return 0;
  }
 }
+char username[10],password[10];
+printHead();
+welcome();
+printHead();
+printf("\n\t\t\t\t Login Screen");
+printf("\n\t\t\t Enter Your Credential");
+printf("\n\n\n\t\tUsername: ");
+scanf("%s",username);
+printf("\n\t\tPassword: ");
+int i;
+i=0;
+do
+{
